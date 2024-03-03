fce0348a2cc78ba9be957daeba21a4e952126fed
CPearl0 <1269596929@qq.com>
2024-03-03 12:37:49 +0800

    turn to forgemdk file struct

--- README.md
+++
@@ -1,2 +0,0 @@
-# Create Dimensional Relics Core
-Mod for the modpack Create Dimensional Relics.
--- src/main/java/com/jasonsimpart/createdimensionalrelicscore/event/EventHandler.java
+++ src/main/java/com/jasonsimpart/createdimensionalrelicscore/event/EventHandler.java
@@ -1,6 +1,11 @@
 package com.jasonsimpart.createdimensionalrelicscore.event;
 
 import com.jasonsimpart.createdimensionalrelicscore.CreateDimensionalRelicsCore;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraftforge.event.entity.EntityJoinLevelEvent;
+import net.minecraftforge.event.level.LevelEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = CreateDimensionalRelicsCore.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
