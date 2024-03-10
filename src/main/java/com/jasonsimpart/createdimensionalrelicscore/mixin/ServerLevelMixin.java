76a59f6519a26c4c6cde3ca2e1ae79dac75d0d06
CPearl0 <1269596929@qq.com>
2024-03-10 22:12:05 +0800

    Change dayTime speed

--- src/main/java/com/jasonsimpart/createdimensionalrelicscore/mixin/ServerLevelMixin.java
+++ src/main/java/com/jasonsimpart/createdimensionalrelicscore/mixin/ServerLevelMixin.java
@@ -1,6 +1,8 @@
 package com.jasonsimpart.createdimensionalrelicscore.mixin;
 
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.dimension.DimensionType;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,7 +14,9 @@ public abstract class ServerLevelMixin {
     @Shadow public abstract void setDayTime(long p_8616_);
 
     @Inject(method = "tickTime", at = @At(value = "RETURN"))
-    public void setNight(CallbackInfo ci) {
-        setDayTime(18000);
+    public void setTime(CallbackInfo ci) {
+        var self = (ServerLevel) (Object) this;
+        if (self.dimension().equals(Level.OVERWORLD))
+            setDayTime(self.getLevelData().getDayTime() + 1L);
     }
 }
